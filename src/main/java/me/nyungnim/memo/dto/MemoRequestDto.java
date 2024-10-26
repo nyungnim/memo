009bb06fbb4e415c43db5960a8564d05c561686c
namalleng <nyu0kim@gmail.com>
2024-10-27 05:58:27 +0900

    feat: Implement Create and Read functionalities with entity, controller, and DTO setup - Added entity classes to define data models - Created controller to handle Create and Read requests - Developed DTOs for data transfer between layers

---
+++ src/main/java/me/nyungnim/memo/dto/MemoRequestDto.java
@@ -0,0 +1,9 @@
+package me.nyungnim.memo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
---
+++ src/main/java/me/nyungnim/memo/dto/MemoResponseDto.java
@@ -0,0 +1,17 @@
+package me.nyungnim.memo.dto;
+
+import lombok.Getter;
+import me.nyungnim.memo.entity.Memo;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String username;
+    private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.username = memo.getUsername();
+        this.contents = memo.getContents();
+    }
+}
